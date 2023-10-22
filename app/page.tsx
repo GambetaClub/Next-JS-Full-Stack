1c933d204ded09a79ae28ea43cdd74f0b2e0c10f
GambetaClub <marigyt.maj@gmail.com>
2023-10-22 17:34:32 -0500

    Just testing.

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
 		queryFn: allPosts,
 		queryKey: ['posts'],
 	})
+	const hello = 'hello'
 	if (error) return error
 	if (isLoading) return 'Loading...'
 	return (
